2824b1546e8d7b53414ea64a4fe1dea333867da5
JAuxance <auxance.jourdan@proton.me>
2025-12-09 18:42:02 +0100

    fix: correct null check and return value in binary_tree_is_leaf function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,17 +1,19 @@
 #include "binary_trees.h"
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
- *
- * node - 
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, 0 otherwise
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-	return;
+	if (!node)
+		return (NULL);
 
-	if (node->left)
-	{
-		
-	}
+	/* if it's a leaf*/
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	/* return sum of NULL node*/
+	return (binary_tree_is_leaf(node->left) + binary_tree_is_leaf(node->right));
 }
