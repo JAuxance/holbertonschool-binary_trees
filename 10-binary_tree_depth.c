2ed70d7b0d03785f458741524c543f76579de735
JAuxance <auxance.jourdan@proton.me>
2025-12-09 22:31:52 +0100

    fix: improve readability of binary_tree_depth function by adding braces

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,7 +12,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->parent == NULL)
+	{
 		return (0);
+	}
 
 	depth = binary_tree_depth(tree->parent);
 	return (depth + 1);
