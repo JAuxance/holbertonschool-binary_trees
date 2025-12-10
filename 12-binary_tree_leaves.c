4cc9b81f7c450d27b438bae4d6bc8a53f4c636d6
Nzoowdev <nzoowcontact@gmail.com>
2025-12-10 12:26:23 +0000

    feat: add binary_tree_balance function to measure the balance factor of a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the leaves
+ *
+ * Return: number of leaves, 0 if tree is NULL
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t roadleft, roadright;
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{	
+	binary_tree_t left_h, right_h;
+	if (!tree)
+		return (0);
+	
+	left_h = height(tree->left);
+	right_h = height(tree->right);
+
+	return ((left_h > right_h) ? left_h : right_h) + 1;
+}
